b6e1633f63fe4f60718e32a61c974834bbc4c264
Jeff Ohrstrom <johrstrom@hotmail.com>
2022-11-27 13:41:36 -0500

    add esbuild javascript pipeline with sass

---
+++ esbuild.config.js
@@ -0,0 +1,27 @@
+const esbuild = require('esbuild');
+const fs = require('fs');
+
+
+// could just glob and pass this in the cli, but glob support is shell dependant
+entryPoints = filesFromDir('app/assets/javascripts');
+
+function filesFromDir(dir) {
+  return fs.readdirSync(dir).map((file) => {
+    if(fs.lstatSync(`${dir}/${file}`).isDirectory()) {
+      return filesFromDir(`${dir}/${file}`);
+    } else {
+      return `${dir}/${file}`;
+    }
+  }).flat(); // only works for 1 subdirectory
+}
+
+esbuild.build({
+  entryPoints: entryPoints,
+  bundle: true,
+  sourcemap: true,
+  format: 'esm',
+  outdir: 'app/assets/builds',
+  external: ['fs'],
+  minify: process.env.RAILS_ENV == 'production' ? true : false,
+}).catch((e) => console.error(e.message));
+
