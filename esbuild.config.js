f8b3677f1965ae9e386419e1e11235bea4ed1e1b
Jeff Ohrstrom <johrstrom@hotmail.com>
2022-12-27 16:20:52 -0500

    really fix watching assets

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,9 +1,13 @@
 const esbuild = require('esbuild');
 const fs = require('fs');
+const { exec } = require('child_process');
 
+
+const assetsDir = 'app/assets'
 
 // could just glob and pass this in the cli, but glob support is shell dependant
-entryPoints = filesFromDir('app/assets/javascripts');
+jsEntryPoints = filesFromDir(`${assetsDir}/javascripts`);
+cssEntryPoints = filesFromDir(`${assetsDir}/stylesheets`);
 
 function filesFromDir(dir) {
   return fs.readdirSync(dir).map((file) => {
@@ -15,14 +19,44 @@ function filesFromDir(dir) {
   }).flat(); // only works for 1 subdirectory
 }
 
-esbuild.build({
-  entryPoints: entryPoints,
-  bundle: true,
-  sourcemap: true,
-  format: 'esm',
-  outdir: 'app/assets/builds',
-  external: ['fs'],
-  minify: process.env.RAILS_ENV == 'production' ? true : false,
-  watch: process.argv.includes('--watch'),
-}).catch((e) => console.error(e.message));
+let firing = false;
 
+function rebuildHandler(eventType, fileName) {
+  if(firing){
+    return;
+  } else {
+    firing = true;
+    railsRebuild();
+    setTimeout(() => { firing = false; }, 400);
+  }
+}
+
+function railsRebuild() {
+  exec('bin/recompile_js', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    console.log(stdout);
+    console.error(stderr);
+  });
+}
+
+// special watch definition so we can invoke the rails pipeline
+if(process.argv.includes('--watch')) {
+  jsEntryPoints.concat(cssEntryPoints).forEach(file => {
+    fs.watch(file, rebuildHandler);
+  });
+} else {
+
+  esbuild.build({
+    entryPoints: jsEntryPoints,
+    bundle: true,
+    sourcemap: true,
+    format: 'esm',
+    outdir: 'app/assets/builds',
+    external: ['fs'],
+    minify: process.env.RAILS_ENV == 'production' ? true : false,
+    watch: false,
+  }).catch((e) => console.error(e.message));
+}
